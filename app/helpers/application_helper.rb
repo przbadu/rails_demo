9eb8113a2aee4d5caf9f7c813bf71839bf61db11
przbadu <pushparaj.badu@gmail.com>
2022-02-10 10:52:12 +0545

    feat: improve transaction listing ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def amount_to_currency(amount)
+    return amount if amount.blank?
+
+    number_to_currency(amount, unit: "#{amount.currency.symbol} ")
+  end
+
+  def amount_colorize(type)
+    return type unless type
+
+    type.to_s == 'income' ? 'text-success' : 'text-danger'
+  end
+
+  def format_date(date)
+    return date unless date
+
+    date.strftime('%b %d, %Y %H:%M %P')
+  end
 end
